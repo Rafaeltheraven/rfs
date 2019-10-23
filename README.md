8ba1442ef76851dfeb2c5c01cc79336727d89515
Rafaeltheraven <rafael.dulfer@gmail.com>
2019-10-23 11:25:49 +0300

    Initial push

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# rfs
+# RFS
 An RSS feed for your FileSystem
+
+RFS will construct an RSS feed describing the last 10 files either modified or 
+created in a given directory. 
+
+To use, copy `config.php.example` to `config.php` and modify as needed. Afterwards,
+simply serve `index.php` in any way you want.
---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+
+header("Content-Type: application/rss+xml; charset=ISO-8859-1");
+
+$configs = include('config.php');
+
+$rssfeed = '<?xml version="1.0" encoding="ISO-8859-1"?>';
+$rssfeed = '<?xml version="1.0" encoding="ISO-8859-1"?>';
+$rssfeed .= '<rss version="2.0">';
+$rssfeed .= '<channel>';
+$rssfeed .= '<title>' . $configs['title'] . '</title>';
+$rssfeed .= '<link>' . $configs['url'] . '</link>';
+$rssfeed .= '<description>' . $configs['description']'</description>';
+$rssfeed .= '<language>en-us</language>'
+
+$root = $configs['root'];
+
+$fileSystemIterator = new fileSystemIterator($root);
+
+$entries = array();
+foreach ($fileSystemIterator as $fileInfo) {
+	$entries[] = (
+		'filename' => $fileInfo -> getFilename(),
+		'mtime' => $fileInfo -> getMTime(),
+		'ctime' => $fileInfo -> getCTime(),
+	);
+}
+
+usort($entries, function($a, $b) {
+	return $b["mtime"] <=> $a["mtime"];
+}) 
+
+$entries = array_slice($entries, 0, 10);
+
+foreach($entries as $entry) {
+	$path = $entry['filename'];
+	$mtime = $entry['mtime'];
+	$ctime = $entry['ctime'];
+	$rssfeed .= "<item>";
+	if ($mtime == $ctime) {
+		$rssfeed .= "<title> File " . $path . " created or permissions modified </title>";
+	} else {
+		$rssfeed .= "<title> File " . $path . " was modified </title>";
+	}
+	$rssfeed .= "<description> For security reasons, no more info is shows. Check out " . $path . " yourself.";
+	$rssfeed .= "<link>" .  $configs['url'] . "</link>";
+	$rssfeed .= "<pubDate>" . date("r", $mtime) . "</pubDate>";
+	$rssfeed .= "</item>";
+}
+
+$rssfeed .= "</channel>";
+$rssfeed .= "</rss>";
+
+echo $rssfeed;
+
+?>
